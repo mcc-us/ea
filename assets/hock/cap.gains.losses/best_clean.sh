c90091301a683b0a05d127d8480e6c7b8549d248
mcc-us <matt.curcio.us@gmail.com>
2024-12-01 13:56:32 -0500

    Video pics from lectures

---
+++ assets/hock/cap.gains.losses/best_clean.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+cat *.txt >> cap.gains.md
+
+file="cap.gains.md"
+
+# Remove lines that start with"
+sed -i '/^Study Unit/d' "$file"
+sed -i '/^This question is/d' "$file"
+
+# Replace TEXT Phrases
+sed -i "s/\"/'/g" "$file"
+sed -i "s/Correct Answer Explanation for /\",\"/g" "$file"
+sed -i 's/\(correct\|wrong\)$//' "$file"
+sed -i "s/United States/US/g" "$file"
+sed -i "s/U\.S\./US/g" "$file"
+
+sed -i 's/^[0-9]\{1,2\}\.*/\"  "/' "$file"
+
+sed -i '/^$/d' "$file"  # Remove empty lines
+echo "Complete"
