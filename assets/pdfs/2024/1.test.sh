1ef95b419df997ba034ef6a29f4172023b6828a7
mcc-us <matt.curcio.us@gmail.com>
2025-03-03 17:27:55 -0500

    updated scrip[ts

--- assets/pdfs/2024/1.test.sh
+++ assets/pdfs/2024/1.test.sh
@@ -17,16 +17,10 @@ for pdf_file in $pdf_files; do
   subject=$(pdfinfo "$pdf_file" | grep "Subject: " | cut -d ':' -f 2-)
 
   # Clean subject: lowercase, remove double spaces, apply substitutions
-  subject=$(echo "$subject" \
-    | tr '[:upper:]' '[:lower:]' \
-    | tr -d '[:punct:]' \
-    | sed 's/application/app/g' \
+  subject=$(echo "$subject" | tr '[:upper:]' '[:lower:]' | sed 's/  */ /g' \
+    | sed 's/application/App/g' \
     | sed 's/u\.s\. return of //g' \
     | sed 's/u\.s\. //g' \
-    | sed 's/ and //g' \
-    | sed 's/,/./g' \
-    | sed 's/, (including //g' \
-    | sed 's/ and losses)//g' \
     | sed 's/instructions for //g' \
     | sed 's/, (for individuals)//g' \
     | sed 's/, (for individuals who use schedule c)//g' \
@@ -34,14 +28,17 @@ for pdf_file in $pdf_files; do
     | sed 's/, for individuals//g' \
     | sed 's/ u\.s\. income tax return for / /g' \
     | sed 's/ and other tax-favored health plans//g' \
-    | sed 's/individual retirement arrangements (iras)/IRAs/g' \
-    | sed 's/ for individuals//g' \
-    | sed 's/ and other tax-favored health plans//g' \
     | tr -d '\r'
   )
 
-  # Append table row to content
-  content+="$filename-$subject.pdf\n"
+  # Remove punctuation and replace with hyphens
+  subject=$(echo "$subject" | sed 's/[[:punct:]]/-/g')
+
+  # Separate words with hyphens
+  subject=$(echo "$subject" | tr ' ' '-')
+
+  # Append table row to content (without '|')
+  content+="$filename $subject\n"
 done
 
 # Write the final content to the markdown file
